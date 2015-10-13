e4e364d872c3d9ebd598e413cc154f6f7dbfe379
L <9693726@student.swin.edu.au>
2015-10-13 19:40:06 +1100

    added more stuff again

--- Test2P/app/src/main/java/com/au/test2p/MainActivity.java
+++ Test2P/app/src/main/java/com/au/test2p/MainActivity.java
@@ -2,19 +2,38 @@ package com.au.test2p;
 
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
     final static String TAG = "whatevs";
-    IntentFilter mIntentFilter;
     private WifiP2pManager manager;
-    private Channel channel;
+    private WifiP2pManager.Channel channel;
+    //WifiP2pDeviceList peerList = new WifiP2pDeviceList();
+    IntentFilter mIntentFilter;
+    private static String address = "66:b3:10:d1:75:d7";
+    private static int port = 8988;
+    //configure the address
+    WifiP2pConfig config = new WifiP2pConfig();
+    static WifiP2pInfo winfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +49,19 @@ public class MainActivity extends AppCompatActivity {
         // I code here
         manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         channel = manager.initialize(this, getMainLooper(), null);
-        //receiver = new Receiver(manager, channel, this);
         manager.initialize(getApplicationContext(), getMainLooper(), null);
-        /*//discover peers
+
+        //discover peers
         manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
             @Override
             public void onSuccess() {
-                Toast.makeText(getApplicationContext(), "Discovery Initiated",
-                        Toast.LENGTH_SHORT).show();
                 if (manager != null) {
-                    blah();
+                    manager.requestPeers(channel, new WifiP2pManager.PeerListListener() {
+                        @Override
+                        public void onPeersAvailable(WifiP2pDeviceList peers) {
+                            new Async(getApplicationContext()).execute();
+                        }
+                    });
                 }
             }
             @Override
@@ -47,10 +69,63 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Discovery Failed : " + reasonCode,
                         Toast.LENGTH_SHORT).show();
             }
-        });*/
-        new Receiver(manager, channel, this);
+        });
     }
-    public void blah(){
-        new Receiver(manager, channel, this);
+}
+class Async extends AsyncTask<Void, Void, String> {
+    private Context context;
+
+    public Async(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8988);
+            Log.d(MainActivity.TAG, "Server: Socket opened");
+            Socket client = serverSocket.accept();
+
+            final File f = new File(Environment.getExternalStorageDirectory() + "/"
+                    + "/wifip2pshared-" + System.currentTimeMillis()
+                    + ".txt");
+
+            File dirs = new File(f.getParent());
+            if (!dirs.exists())
+                dirs.mkdirs();
+            f.createNewFile();
+
+            Log.d(MainActivity.TAG, "server: copying files " + f.toString());
+            InputStream inputstream = client.getInputStream();
+            copyFile(inputstream, new FileOutputStream(f));
+
+            //InputStream inputstream = client.getInputStream();
+            Log.d(MainActivity.TAG, "Server: connection done");
+            Log.e(MainActivity.TAG, "inputstream : " + inputstream);
+            serverSocket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static boolean copyFile(InputStream inputStream, OutputStream out) {
+        byte buf[] = new byte[1024];
+        int len;
+        long startTime=System.currentTimeMillis();
+
+        try {
+            while ((len = inputStream.read(buf)) != -1) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+            inputStream.close();
+            long endTime=System.currentTimeMillis()-startTime;
+            Log.v("", "Time taken to transfer all bytes is : " + endTime);
+
+        } catch (IOException e) {
+            Log.d(MainActivity.TAG, e.toString());
+            return false;
+        }
+        return true;
     }
 }
